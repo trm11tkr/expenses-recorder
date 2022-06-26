d907d7a763e0664855713ccad0e9f1e0c81ef680
森田陸斗 <mrmrt1121@gmail.com>
2022-06-27 00:28:33 +0900

    Chartをビューに表示

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 import '../models/transaction.dart';
+import './chart_bar.dart';
 
 class Chart extends StatelessWidget {
   const Chart({Key? key, required this.recentTransactions}) : super(key: key);
@@ -26,18 +27,35 @@ class Chart extends StatelessWidget {
     });
   }
 
+  double get totalSpending {
+    return groupedTransactionValues.fold(0.0, (sum, item) {
+      return sum + item['amount'];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    print(groupedTransactionValues);
     return Card(
       elevation: 6,
-      margin:const EdgeInsets.all(20),
-      child: Row(
-        children: groupedTransactionValues.map(
-          (data) {
-            return Text('${data['day']}: ${data['amount']}');
-          },
-        ).toList(),
+      margin: const EdgeInsets.all(20),
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: groupedTransactionValues.map(
+            (data) {
+              return Flexible(
+                fit: FlexFit.tight,
+                child: ChartBar(
+                    label: data['day'],
+                    spendingAmount: data['amount'],
+                    spendingPctOfTotal: totalSpending == 0.0
+                        ? 0.0
+                        : data['amount'] / totalSpending),
+              );
+            },
+          ).toList(),
+        ),
       ),
     );
   }
---
+++ lib/widgets/chart_bar.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class ChartBar extends StatelessWidget {
+  const ChartBar(
+      {Key? key,
+      required this.label,
+      required this.spendingAmount,
+      required this.spendingPctOfTotal})
+      : super(key: key);
+
+  final String label;
+  final int spendingAmount;
+  final double spendingPctOfTotal;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        FittedBox(child: Text('¥${spendingAmount.toString()}')),
+        const SizedBox(
+          height: 4,
+        ),
+        SizedBox(
+          height: 60,
+          width: 10,
+          child: Stack(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey, width: 1.0),
+                  color: const Color.fromRGBO(220, 220, 220, 1),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              FractionallySizedBox(
+                heightFactor: spendingPctOfTotal,
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.circular(10)),
+                ),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(
+          height: 4,
+        ),
+        Text(label),
+      ],
+    );
+  }
+}

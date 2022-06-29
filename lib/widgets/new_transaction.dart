0e9035ef640e7e30d0731670af5665ce5d937239
森田陸斗 <mrmrt1121@gmail.com>
2022-06-29 21:19:58 +0900

    キーボードでフォームが隠れるバグを修正

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -52,7 +52,11 @@ class _NewTransactionState extends State<NewTransaction> {
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Container(
-        padding: const EdgeInsets.all(10),
+        padding: EdgeInsets.only(
+            top: 10,
+            left: 10,
+            right: 10,
+            bottom: MediaQuery.of(context).viewInsets.bottom + 20),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
